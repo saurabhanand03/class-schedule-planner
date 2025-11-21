50c440acbfc3d3247784d48f084c6da1a7872273
Saurabh <asaurabh2003@gmail.com>
2025-11-21 04:26:36 -0600

    Add browser cookie functionality

--- TODO.md
+++ TODO.md
@@ -1,5 +1,5 @@
-* Store class list as browser cookie
 * Fix padding issue when dragging (https://stackoverflow.com/questions/62562944/how-do-i-maintain-proper-spacing-after-my-element-is-dropped-using-react-beautif)
 * Export/Import option
+* Refactor SemesterList.jsx
 * Share option
 * Automatic reading
--- src/components/SemesterList.jsx
+++ src/components/SemesterList.jsx
@@ -1,7 +1,8 @@
 import { DragDropContext } from 'react-beautiful-dnd';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import classes from '../data/GT_Classes_2.json';
 import Semester from "./Semester";
+import Cookies from 'js-cookie';
 
 const SemesterList = () => {
     const [semesters, setSemesters] = useState({
@@ -20,24 +21,66 @@ const SemesterList = () => {
         unassigned: classes,
     });
 
-    const [calendarMode, setCalendarMode] = useState(false);
-    const toggleCalendarMode = () => setCalendarMode(!calendarMode);
+    useEffect(() => {
+        if (!Cookies.get('semester1')) {
+            Object.entries(semesters).slice(0,-1).forEach(([semesterId, semesterClasses]) => {
+                Cookies.set(semesterId, JSON.stringify(semesterClasses.map(classItem => classItem.code)));
+            });
+            console.log('Initial class data saved to Cookies.');
+            console.log(Cookies.get());
+        } else {
+            Object.keys(semesters).slice(0,-1).forEach((semesterId) => {
+                const classCodes = JSON.parse(Cookies.get(semesterId));
+                const semesterClasses = classCodes.map(code => 
+                    classes.find(classItem => classItem.code === code)
+                ).filter(classItem => classItem !== undefined);
+                console.log(`${semesterId}:`, semesterClasses);
+                
+                setSemesters(prevSemesters => ({
+                    ...prevSemesters,
+                    [semesterId]: semesterClasses,
+                    unassigned: prevSemesters.unassigned.filter(classItem => !classCodes.includes(classItem.code)),
+                }));
+            });
+        }
 
-    const [startYear, setStartYear] = useState(new Date().getFullYear());
-    const [startSemester, setStartSemester] = useState( new Date().getMonth() < 5 ? 0 : 
-                                                        new Date().getMonth() < 7 ? 1 : 2);
+        console.log('Semesters after loading from Cookies:', semesters);
+    }, []);
+
+    const [calendarMode, setCalendarMode] = useState(Cookies.get('calendarMode') === 'true' ? true : false);
+    const toggleCalendarMode = () => {
+        setCalendarMode(!calendarMode);
+        Cookies.set('calendarMode', !calendarMode);
+    };
+
+    const [startYear, setStartYear] = useState(Cookies.get('startYear') ? 
+                                            parseInt(Cookies.get('startYear')) :
+                                            new Date().getFullYear());
+    const [startSemester, setStartSemester] = useState(Cookies.get('startSemester') ? 
+                                                    parseInt(Cookies.get('startSemester')) : 
+                                                    (
+                                                        new Date().getMonth() < 5 ? 0 : 
+                                                        new Date().getMonth() < 7 ? 1 : 
+                                                        2
+                                                    ));
 
     const increaseStartSemester = () => startSemester < 2 ? (
-        setStartSemester(startSemester + 1)
+        setStartSemester(startSemester + 1),
+        Cookies.set('startSemester', startSemester + 1)
     ) : (
         setStartSemester(0),
-        setStartYear(startYear + 1)
+        setStartYear(startYear + 1),
+        Cookies.set('startSemester', 0),
+        Cookies.set('startYear', startYear + 1)
     );
     const decreaseStartSemester = () => startSemester > 0 ? (
-        setStartSemester(startSemester - 1)
+        setStartSemester(startSemester - 1),
+        Cookies.set('startSemester', startSemester - 1)
     ) : (
         setStartSemester(2),
-        setStartYear(startYear - 1)
+        setStartYear(startYear - 1),
+        Cookies.set('startSemester', 2),
+        Cookies.set('startYear', startYear - 1)
     );
 
     function handleOnDragEnd(result) {
@@ -50,30 +93,36 @@ const SemesterList = () => {
 
         // Reorder classes within the same semester
         if (source.droppableId === destination.droppableId) {
-        const semester = semesters[source.droppableId];
-        const reorderedClasses = Array.from(semester);
-        const [draggedClass] = reorderedClasses.splice(source.index, 1);
-        reorderedClasses.splice(destination.index, 0, draggedClass);
+            const semester = semesters[source.droppableId];
+            const reorderedClasses = Array.from(semester);
+            const [draggedClass] = reorderedClasses.splice(source.index, 1);
+            reorderedClasses.splice(destination.index, 0, draggedClass);
 
-        setSemesters({
-            ...semesters,
-            [source.droppableId]: reorderedClasses,
-        });
+            setSemesters({
+                ...semesters,
+                [source.droppableId]: reorderedClasses,
+            });
         }
 
         // Move class between semesters
         else {
-        const sourceSemester = semesters[source.droppableId];
-        const destinationSemester = semesters[destination.droppableId];
-        const [draggedClass] = sourceSemester.splice(source.index, 1);
-        destinationSemester.splice(destination.index, 0, draggedClass);
+            const sourceSemester = semesters[source.droppableId];
+            const destinationSemester = semesters[destination.droppableId];
+            const [draggedClass] = sourceSemester.splice(source.index, 1);
+            destinationSemester.splice(destination.index, 0, draggedClass);
 
-        setSemesters({
-            ...semesters,
-            [source.droppableId]: sourceSemester,
-            [destination.droppableId]: destinationSemester,
-        });
+            setSemesters({
+                ...semesters,
+                [source.droppableId]: sourceSemester,
+                [destination.droppableId]: destinationSemester,
+            });
+
+            // Update Cookies
+            Object.entries(semesters).slice(0,-1).forEach(([semesterId, semesterClasses]) => {
+                Cookies.set(semesterId, JSON.stringify(semesterClasses.map(classItem => classItem.code)));
+            });
         }
+
     }
 
     return (
